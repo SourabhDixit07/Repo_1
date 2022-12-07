9b81f90c17bbcc9dac9d0e7f17b83ef60a86a85e
SourabhDixit07 <118927494+SourabhDixit07@users.noreply.github.com>
2022-12-07 16:47:20 +0530

    Add files via upload

---
+++ miniProject.py
@@ -0,0 +1,31 @@
+#Saurabh project
+import random
+
+MAX_LEN =int(input("Enter length of your password: "))
+ 
+# declare arrays of the character that we need in out password
+
+DIGITS = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'] 
+LOCASE_CHARACTERS = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
+                     'i', 'j', 'k', 'm', 'n', 'o', 'p', 'q',
+                     'r', 's', 't', 'u', 'v', 'w', 'x', 'y',
+                     'z']
+ 
+UPCASE_CHARACTERS = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
+                     'I', 'J', 'K', 'M', 'N', 'O', 'P', 'Q',
+                     'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y',
+                     'Z']
+ 
+SYMBOLS = ['@', '#', '$', '%', '=', ':', '?', '.', '/', '|', '~', '>',
+           '*', '(', ')', '<']
+ 
+
+COMBINED_LIST = DIGITS + UPCASE_CHARACTERS + LOCASE_CHARACTERS + SYMBOLS
+ 
+temp=random.sample(COMBINED_LIST,MAX_LEN)
+
+password = ""
+for x in temp:
+        password = password + x
+
+print("Your Password is: ",password)
